9de98058ae25d7941316ffd368e1d02363f97ced
Unknown <ziyang@exocortex.com>
2016-11-29 14:05:51 -0500

    change host to cliara.io

--- conf.js
+++ conf.js
@@ -4,7 +4,7 @@ var env =opts.env||process.env.YOUR_APP_ENV ||'dev';
 
 var conf = cc(opts,
 	cc.env('clara_'),{
-	host:'https://10.0.3.178',
+	host:'https://clara.io',
 	client_id:'YOUR_CLIENT_ID',
 	client_secret: 'YOUR_CLIENT_SECRET',
 	redirect_uri:'http://localhost:8080/callback',
