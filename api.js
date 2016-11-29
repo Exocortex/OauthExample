40b89a6b7f6ece9e025735a77c223185cd041c6e
Wayne Larsen <wayne@larsen.st>
2016-11-29 17:17:51 -0500

    Update conf and dependencies

--- api.js
+++ api.js
@@ -1,20 +1,21 @@
 var request=require('request');
 var conf = require('./conf');
 
-var sendReq = function sendReq(url, method, req, callback){
+var sendReq = function sendReq(url, method, authHeader, body, callback){
+  console.log(' - ', method, url);
+
+  var headers = { 'Content-Type': 'application/json' };
+  if (authHeader) headers.authorization = authHeader;
+
   request({
     uri: url,
     rejectUnauthorized: false,
     method: method,
-    headers: {
-      Authorization: req.headers.authorization,
-      'Content-Type': 'application/json',
-    },
-    form: req.body
-  },
-  function(err, response, body) {
+    headers: headers,
+    form: body
+  }, function(err, response, body) {
     if (err) return callback(err);
-    if (response.statusCode !== 200) return callback(response.statusCode, body);
+    if (response && response.statusCode !== 200) return callback(response.statusCode, body);
     try {
       const result = JSON.parse(body);
       return callback(null, result);
@@ -25,59 +26,35 @@ var sendReq = function sendReq(url, method, req, callback){
 }
 
 var refreshToken = function(token,callback){
-  var form = {
+  var body = {
     client_id: conf.get('client_id'),
     client_secret: conf.get('client_secret'),
-    redirect_uri:conf.get('redirect_uri'),
     grant_type: 'refresh_token',
     refresh_token: token.refresh_token,
   };
-  var req={};
-  req.body = form;
-  req.headers = {
-    authorization:'Basic',
-    'Content-Type': 'application/json',
-  };
-  sendReq(conf.get('host')+"/oauth/token",'POST', req, function(err,result){
-    if(err && typeof err !== 'number') return callback(err);
-    if(err) return callback(err);
-    callback(null,result);
-  });
+  sendReq(conf.get('host')+"/oauth/token", 'POST', null, body, callback);
 }
 
-exports.oauthSucess = function oauthSucess(accessToken, refreshToken, profile, cb) {
-  var token = {};
-  token.access_token = accessToken;
-  token.refresh_token = refreshToken;
-  cb(null,token);
-}
-
-exports.callApi = function(token,url,method,callback){
-  var call = function(token,url,method,cb){
-    var req = {};
-    req.headers = {
-      authorization:'Bearer '+token.access_token,
-      'Content-Type': 'application/json',
-    };
-    req.body = {};
-    sendReq(url,method,req,function(err,result){
-      if(err && typeof err !== 'number') return cb(err);
-      if(err) return cb(err);
-      return cb(null,result);
+exports.callApi = function(tokens, url, method, callback) {
+  var call = function(tokens, url, method, cb){
+    sendReq(url, method, 'Bearer '+tokens.access_token, {}, function(err, result){
+      if (err && typeof err !== 'number') return cb(err);
+      if (err) return cb(err);
+      return cb(null, result);
     });
-
   };
 
-  call(token,url,method,function(err,result){
+  call(tokens, url, method, function(err, result) {
+    if (!err || err !== 401) return callback(err, result);
 
-    if(!err || err !== 401) return callback(null,result);
+    refreshToken(tokens, function(err, newTokens) {
+      if (err) return callback(err);
 
-    refreshToken(token,function(err,newToken){
-      if(err) return callback(err);
-      call(newToken,url,method,function(err,result){
-        if (err && err === 401) return console.log('Error fetching new token');
-        callback(null,result,newToken);
-      })
+      console.log('Refreshed Tokens');
+
+      call(newTokens, url, method, function(err, result) {
+        callback(err, result, newTokens);
+      });
     })
   })
 }
--- conf.js
+++ conf.js
@@ -1,13 +1,10 @@
 var cc = require('config-chain');
-var opts = require('optimist').argv;
-var env =opts.env||process.env.YOUR_APP_ENV ||'dev';
 
-var conf = cc(opts,
-	cc.env('clara_'),{
+var defaults = {
 	host:'https://clara.io',
 	client_id:'YOUR_CLIENT_ID',
 	client_secret: 'YOUR_CLIENT_SECRET',
 	redirect_uri:'http://localhost:8080/callback',
-});
+}
 
-module.exports = conf;
+module.exports = cc(cc.env(''), defaults);
